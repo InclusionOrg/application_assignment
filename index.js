90838bb61c1ce5e7e8395d84b6444fdd0c709be2
Shahid <shahid.sarker@gmail.com>
2019-05-19 13:45:35 -0400

    Add missing random-movie-quotes module

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000;
 
-app.use(express.static('public'));
+// Include random-movie-quotes as quotes
+const quotes = require("random-movie-quotes");
 
-app.get('/quote', (req, res) => {
+app.use(express.static("public"));
+
+app.get("/quote", (req, res) => {
   res.json({
     quote: quotes.getQuote()
   });
 });
-
 
 app.listen(port, () => console.log(`Awesome app listening on port ${port}!`));
