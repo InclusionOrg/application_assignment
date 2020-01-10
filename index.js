8ed0c87a2b477640acc3f6b263c6f44eb6d3489c
Kevin Wang <kcwang06@gmail.com>
2020-01-10 16:56:03 -0500

    require node module and installed relevant dependencies

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const quotes = require('random-movie-quotes')
 
 app.use(express.static('public'));
 
@@ -9,6 +10,7 @@ app.get('/quote', (req, res) => {
     quote: quotes.getQuote()
   });
 });
+console.log(quotes.getQuote())
 
 
 app.listen(port, () => console.log(`Awesome app listening on port ${port}!`));
